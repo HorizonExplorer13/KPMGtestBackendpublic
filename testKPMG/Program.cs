0c0387a40614c8d90feaed80ff2b7f37df36178d
David <DavidSaidiza0613@outlook.com>
2025-10-07 20:14:46 -0500

    thirdcommit

--- testKPMG/Program.cs
+++ testKPMG/Program.cs
@@ -15,6 +15,19 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    try
+    {
+        dbContext.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        throw;
+    }
+
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
