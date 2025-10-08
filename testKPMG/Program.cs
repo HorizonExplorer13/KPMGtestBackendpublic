2ad9200d434d3df9bbb91505c7315b667427b3c1
David <DavidSaidiza0613@outlook.com>
2025-10-08 11:54:34 -0500

    Fourth commit

--- testKPMG/Program.cs
+++ testKPMG/Program.cs
@@ -30,13 +30,13 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 app.UseMiddleware<ErrorHandlingMiddleware>();
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
